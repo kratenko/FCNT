2a0a56c95067970a9f8564803545eb384dcd6f66
Peer Springstübe <3springs@informatik.uni-hamburg.de>
2016-12-14 12:38:23 +0100

    Removed pu changes (GPU only now)

--- util/switch_pu.m
+++ util/switch_pu.m
@@ -1,13 +1,18 @@
 function [ output_args ] = switch_pu( pu )
-    
-    if strcmp(pu, 'cpu')
-        fprintf('Switching to CPU\n');
-        caffe('set_mode_cpu');
-    elseif strcmp(pu, 'gpu')
-        fprintf('Switching to GPU\n');
-        caffe('set_mode_gpu');
+
+    if 1
+        fprintf('Not switching, GPU only!\n');
     else
-        error('Unknown PU');
+
+        if strcmp(pu, 'cpu')
+            fprintf('Switching to CPU\n');
+            caffe('set_mode_cpu');
+        elseif strcmp(pu, 'gpu')
+            fprintf('Switching to GPU\n');
+            caffe('set_mode_gpu');
+        else
+            error('Unknown PU');
+        end
     end
 
 end
